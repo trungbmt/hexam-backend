434226ba5efb112e0571b783bf26e5f68d453e26
trungbmt <phamductrungbmt@gmail.com>
2021-09-26 11:23:33 +0700

    update register form

--- routes.py
+++ routes.py
@@ -1,16 +1,20 @@
+from flask import flash, render_template, url_for, redirect
 from forms import RegistrationForm, LoginForm
-from app import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('templates/home.html')
+    return render_template('home.html')
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     registerForm = RegistrationForm()
+    
+    if registerForm.validate_on_submit():
+        flash(f'Tạo tài khoản {registerForm.username.data} thành công!', 'success')
+        return redirect(url_for('home'))
     return render_template('auth/register.html', form=registerForm, title="Đăng ký")
 
 
